aa4eb0cd8b8842067e913c03feecc17c180a5564
xuhuiyang <xuhuiyang@persagy.com>
2022-06-29 11:03:25 +0800

    upgrade

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+read -p "请输入版本号: " version
+echo "版本号为：" $version
+
+echo "start maven build"
+
+mvn clean package -f springboot-demo/pom.xml
+
+echo "start docker build"
+
+docker build -t 192.168.50.100:1180/watson/springboot-demo:$version .
+
+echo "start docker push"
+
+docker login 192.168.50.100:1180 -u admin -p Harbor12345
+
+docker push 192.168.50.100:1180/watson/springboot-demo:$version
