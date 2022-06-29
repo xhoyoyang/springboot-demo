59a321c956b2510832c70e88c9a77fe11d1ce21a
xuhuiyang <xuhuiyang@persagy.com>
2022-06-29 11:28:23 +0800

    upgrade

--- build.sh
+++ build.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-read -p "请输入版本号: " version
+read -p "请输入版本号: ' version
 
-echo "start git pull"
+echo 'start git pull'
 
 git pull
 
-echo "start maven build\n"
+echo 'start maven build\n'
 
 mvn clean package
 
-echo "start docker build\n"
+echo 'start docker build\n'
 
 docker build -t 192.168.50.100:1180/watson/springboot-demo:$version .
 
-echo "start docker push\n"
+echo 'start docker push\n'
 
 docker login 192.168.50.100:1180 -u admin -p Harbor12345
 
 docker push 192.168.50.100:1180/watson/springboot-demo:$version
 
-echo "successful"
+echo 'successful'
