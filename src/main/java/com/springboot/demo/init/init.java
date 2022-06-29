e041948a8f6c52442df196e648a2beee40656b75
xuhuiyang <xuhuiyang@persagy.com>
2022-06-29 17:38:23 +0800

    upgrade

--- src/main/java/com/springboot/demo/init/init.java
+++ src/main/java/com/springboot/demo/init/init.java
@@ -2,6 +2,7 @@ package com.springboot.demo.init;
 
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.lang.UUID;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
  * Created by huiyang on 2022-06-29
  */
 @Component
+@Slf4j
 public class init implements CommandLineRunner {
 
     @Autowired
@@ -20,6 +22,7 @@ public class init implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         while (true){
+            log.info(DateUtil.now());
             redisTemplate.opsForValue().set(UUID.fastUUID().toString(true), DateUtil.now());
             Thread.sleep(1000);
         }
