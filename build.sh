e6a92c7234f41d0dc2223d53e7f13059c16153a9
xuhuiyang <xuhuiyang@persagy.com>
2022-06-29 11:39:48 +0800

    upgrade

--- build.sh
+++ build.sh
@@ -24,9 +24,9 @@ docker push 192.168.50.100:1180/watson/springboot-demo:$version
 echo 'start k8s updateImageTag\n'
 
 curl -X PUT \
-    -H "content-type: application/json" \
-    -H "Cookie: KuboardUsername=admin; KuboardAccessKey=yx4wwakfd4xp.izddjf4fty65yd8m4i6ehwyf3xwezmik" \
+    -H 'content-type: application/json' \
+    -H 'Cookie: KuboardUsername=admin; KuboardAccessKey=yx4wwakfd4xp.izddjf4fty65yd8m4i6ehwyf3xwezmik \
     -d '{"kind":"deployments","namespace":"default","name":"springboot-demo","images":{"192.168.50.100:1180/watson/springboot-demo":"192.168.50.100:1180/watson/springboot-demo:$version"}}' \
-    "https://www.xhoyoyang.top:30080/kuboard-api/cluster/default/kind/CICDApi/admin/resource/updateImageTag"
+    'https://www.xhoyoyang.top:30080/kuboard-api/cluster/default/kind/CICDApi/admin/resource/updateImageTag'
 
 echo 'successful'
