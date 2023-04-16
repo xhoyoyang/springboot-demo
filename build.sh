53bfd26bcb64b56b33f3a55e09aeb05cadcfb038
watson <xhoyoyang@gmail.com>
2023-04-16 21:49:20 +0800

    暂存

--- build.sh
+++ build.sh
@@ -5,25 +5,28 @@ version=$(cat /proc/sys/kernel/random/uuid | cksum | awk -F ' ' '{print $1}')
 
 echo "build version:"$version
 
-echo "start git pull"
+echo"[step 1 -------------------------------------------git pull]"
 
 git pull
 
-echo "start maven build"
+echo"[step 2 -------------------------------------------maven build]"
 
 mvn clean package
 
-echo "start docker build"
+echo"[step 3 -------------------------------------------docker build]"
 
 docker build -t 192.168.50.100:1180/watson/springboot-auth:$version .
 
-echo "start docker login"
+echo"[step 4 -------------------------------------------docker login]"
+
 
 docker login 192.168.50.100:1180 -u admin -p Harbor12345
 
-echo "start docker push"
+echo"[step 5 -------------------------------------------docker push inamge]"
 
 docker push 192.168.50.100:1180/watson/springboot-auth:$version
+
+echo"[step 6 -------------------------------------------update container]"
 
 echo "start k8s updateImageTag"
 
@@ -33,8 +36,8 @@ curl -X PUT \
     -d '{"kind":"deployments","namespace":"default","name":"springboot-auth","images":{"192.168.50.100:1180/watson/springboot-auth":"192.168.50.100:1180/watson/springboot-auth:'$version'"}}' \
     "192.168.50.10:30080/kuboard-api/cluster/default/kind/CICDApi/admin/resource/updateImageTag"
 
-echo "docker remove image"
+echo"[step 7 -------------------------------------------docker remove image]"
 
 docker rmi 192.168.50.100:1180/watson/springboot-auth:$version
 
-echo 'successful'
+echo"[step 8 -------------------------------------------done]"
